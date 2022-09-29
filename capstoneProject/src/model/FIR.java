f568eb3fad614b1fb553a179cba5ee9a02c81c41
rinku <sarkr72@mail.sunysuffolk.edu>
2022-09-28 20:47:26 -0400

    made base classes

---
+++ capstoneProject/src/model/FIR.java
@@ -0,0 +1,26 @@
+package model;
+
+public class FIR {
+    
+    private String name_of_policeStation;
+    private String name_of_district;
+    private String subject;
+    private String Fir_no;
+    private String date;
+    private String date_of_occurance;
+    private String date_of_info_received;
+    private String place_of_occurance;
+    private String distance_from_ps;
+    private String address_of_occurance;
+    private String complainant_fName;
+    private String complainant_lName;
+    private String complainant_address;
+    private String complainant_dob;
+    private String complainant_sex;
+    private String complainant_phone;
+    private String complainant_occupation;
+    private String details_of_suspected;
+    private String fir_content;
+    private String deleteIt;
+    
+}
