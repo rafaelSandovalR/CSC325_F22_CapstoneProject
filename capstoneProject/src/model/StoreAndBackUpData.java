9f9ea41aa40f6a391be262b6b371c0a314e1827e
rinku <sarkr72@mail.sunysuffolk.edu>
2022-10-17 00:05:21 -0400

    Made View complaint history controller and fxml file

--- capstoneProject/src/model/StoreAndBackUpData.java
+++ capstoneProject/src/model/StoreAndBackUpData.java
@@ -6,12 +6,21 @@ public class StoreAndBackUpData {
 
     private static TreeMap<String, Officer> officerAccounts;
     private static TreeMap<String, Criminal> criminals;
+    private static TreeMap<String, Complaint> complaintsHistory;
 
     public StoreAndBackUpData() {
         officerAccounts = new TreeMap<>();
         criminals = new TreeMap<>();
+        complaintsHistory = new TreeMap<>();
     }
 
+    public void addComplaint(String id, Complaint complaint){
+        complaintsHistory.put(id, complaint);
+    }
+    
+    public static TreeMap<String, Complaint> getComplaints(){
+        return complaintsHistory;
+    }
     public void addOfficer(String badgeN, Officer user) {
         officerAccounts.put(badgeN, user);
     }
